3421674715d116754688d03d77cd230410cf55ac
robertsumanaru <94854114+robertsumanaru@users.noreply.github.com>
2022-07-03 22:45:45 +0300

    First 7 ep

---
+++ blog/resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My first post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam vel quam elementum pulvinar. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc. Massa sapien faucibus et molestie ac feugiat sed. Arcu dictum varius duis at consectetur lorem donec massa sapien. In massa tempor nec feugiat nisl pretium fusce. Blandit volutpat maecenas volutpat blandit aliquam etiam erat velit. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Id interdum velit laoreet id donec ultrices. Lectus arcu bibendum at varius vel. Odio aenean sed adipiscing diam donec adipiscing. Risus nullam eget felis eget nunc lobortis mattis.</p>
+    </article>
+    <a href="/">Back</a>
+</body>
